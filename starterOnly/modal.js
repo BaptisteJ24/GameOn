bbbb9ef815a290170676278ca4b2466aa5d43fa4
BaptisteJ24 <baptistejan@outlook.fr>
2023-01-18 17:28:58 +0100

    Added form checks

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,9 +10,9 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground"); // modal background
 const modalBtn = document.querySelectorAll(".modal-btn"); // modal buttons
-const formData = document.querySelectorAll(".formData"); // form data
-
-const closeBtn = document.querySelector(".close"); // close button
+const modalForm = document.querySelector(".modal-body form"); // modal form
+const closeCross = document.querySelector(".close"); // close modal
+const closeBtn = document.querySelector(".btn-close"); // close modal button
 const firstName = document.getElementById("first"); // first name
 const lastName = document.getElementById("last"); // last name
 const email = document.getElementById("email"); // email
@@ -20,6 +20,64 @@ const birthdate = document.getElementById("birthdate"); // birthdate
 const quantity = document.getElementById("quantity"); // quantity
 const submitBtn = document.getElementById("btn-submit"); // submit button
 
+const radioBtns = Array.from(document.getElementsByName("location")); // radio buttons
+const radioFormData = document.getElementsByClassName("formData--radioBtn");
+
+const checkbox_UsingConditions = document.getElementById("checkbox1") // checkbox conditions of use
+const checkbox_Newsletter = document.getElementById("checkbox2") // checkbox
+
+
+// Regex object
+const regex = {
+  text: /^[a-zA-Z]+[-]?[a-zA-Z]+$/,
+  email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+  date: /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/,
+  number: /^[0-9]{1,2}$/
+}
+
+// formData object for each input field in the form : value, type, error, id, valid, regex
+let formData = {
+  firstName: {
+    value: firstName.value,
+    type: firstName.type,
+    error: firstName.dataset.error,
+    id: firstName,
+    valid: false,
+    regex: regex['text']
+  },
+  lastName: {
+    value: lastName.value,
+    type: lastName.type,
+    error: lastName.dataset.error,
+    id: lastName,
+    valid: false,
+    regex: regex['text']
+  },
+  email: {
+    value: email.value,
+    type: email.type,
+    error: email.dataset.error,
+    id: email,
+    valid: false,
+    regex: regex['email']
+  },
+  birthdate: {
+    value: birthdate.value,
+    type: birthdate.type,
+    error: birthdate.dataset.error,
+    id: birthdate,
+    valid: false,
+    regex: regex['date']
+  },
+  quantity: {
+    value: quantity.value,
+    type: quantity.type,
+    error: quantity.dataset.error,
+    id: quantity,
+    valid: false,
+    regex: regex['number']
+  }
+}
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -30,92 +88,92 @@ function launchModal() {
 }
 
 // close modal event
+closeCross.addEventListener("click", closeModal);
 closeBtn.addEventListener("click", closeModal);
 
 // close modal form
 function closeModal() {
-  modalbg.style.display = "none";
+  modalbg.style.display = "none"; // hide modal
+  modalForm.reset(); // reset form
+  Object.keys(formData).forEach((field) => { // loop through formData object
+    formData[field].id.style.border = "none"; // reset border of input fields
+  });
+  modalForm.style.display = "block";
+  document.querySelector(".modal-body-sucess").style.display = "none";
 }
 
-function validateFirstName() { // validate first name : at least 2 characters / not empty
-  let regex = /^[a-zA-Z]{2,}$/;
-  if (!regex.test(firstName.value)) {
-    firstName.style.borderColor = "red";
-    firstName.style.borderWidth = "2px";
-    return false;
+// event listener for each input field and green border if is valid
+Object.keys(formData).forEach((field) => { // loop through formData object
+  formData[field].id.addEventListener("focusout", (e) => { // event listener for each input field
+    validateField(e.target, formData[field]); // validate input field
+  });
+});
+
+function validateField(field, fieldData) {
+  if (fieldData.regex.test(field.value)) {
+    field.style.border = "2px solid green";
+    fieldData.valid = true;
   } else {
-    firstName.style.borderColor = "green";
-    firstName.style.borderWidth = "2px";
-    return true;
+    field.style.border = "none";
+    fieldData.valid = false;
   }
 }
 
-firstName.addEventListener("input", validateFirstName); // when user is typing in the first name input, the function is called
-
-
-function validateLastName() { // validate last name : at least 2 characters / not empty
-  let regex = /^[a-zA-Z]{2,}$/;
-  if (!regex.test(lastName.value)) {
-    lastName.style.borderColor = "red";
-    lastName.style.borderWidth = "2px";
-    return false;
-  } else {
-    lastName.style.borderColor = "green";
-    lastName.style.borderWidth = "2px";
-    return true;
+// initialize formData with the values entered in the form
+const initFormData = () => {
+  for (let object in formData) {
+    formData[object].value = formData[object].id.value;
   }
 }
 
-lastName.addEventListener("input", validateLastName); // when user is typing in the last name input, the function is called
+// validate form and display error message if necessary. Else, display success message.
+const validateForm = (e) => {
+  e.preventDefault();
+  initFormData();
 
+  let validAll = 0;
 
-function validateEmail() { // validate email : verify if the email is valid.
-  let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  if (!regex.test(email.value)) {
-    email.style.borderColor = "red";
-    email.style.borderWidth = "2px";
-    return false;
-  } else {
-    email.style.borderColor = "green";
-    email.style.borderWidth = "2px";
-    return true;
+  for (let object in formData) {
+    formData[object].id.style.border = 'none';
+    if (!formData[object].regex.test(formData[object].value)) {
+      formData[object].id.style.border = '2px solid red';
+      return false;
+    }
+    else {
+      formData[object].id.style.border = '2px solid green';
+      validAll++;
+    }
   }
+
+  if (validAll == 5) {
+    radioBtns.forEach(function (radioBtn) {
+      if (radioBtn.checked) {
+        validAll++;
+        radioBtn.labels[0].querySelector(".checkbox-icon").classList.toggle("checkbox-icon--error", false);
+      }
+    });
+    if (validAll != 6) {
+      radioBtns.forEach(function (radioBtn) {
+        radioBtn.labels[0].querySelector(".checkbox-icon").classList.toggle("checkbox-icon--error", true);
+      });
+      radioFormData.innerHTML += "Vous devez choisir un tournoi auquel participer.";
+      return false;
+    }
+  }
+
+  if (validAll == 6 && checkbox_UsingConditions.checked == true) {
+    modalForm.style.display = "none";
+    document.querySelector(".modal-body-sucess").style.display = "block";
+    if (checkbox_Newsletter.checked == true) {
+      console.log(document.querySelector(".modal-body-sucess").querySelector(".modal-text"));
+      document.querySelector(".modal-body-sucess").querySelector(".modal-text").style.display = "block";
+    }
+  }
+
+
 }
 
-email.addEventListener("input", validateEmail); // when user is typing in the email input, the function is called
-
-
-function validateBirthdate() { // validate birthdate : verify if the birthdate is valid.
-  let regex = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
-  if (!regex.test(birthdate.value)) {
-    birthdate.style.borderColor = "red";
-    birthdate.style.borderWidth = "2px";
-    return false;
-  } else {
-    birthdate.style.borderColor = "green";
-    birthdate.style.borderWidth = "2px";
-    return true;
-  }
-}
-
-birthdate.addEventListener("change", validateBirthdate); // when user change the birthdate input, the function is called.
-
-function validateQuantity() { // validate quantity : verify if the quantity is valid.
-  let regex = /^[0-9]{1,2}$/;
-  if (!regex.test(quantity.value)) {
-    quantity.style.borderColor = "red";
-    quantity.style.borderWidth = "2px";
-    return false;
-  } else {
-    quantity.style.borderColor = "green";
-    quantity.style.borderWidth = "2px";
-    return true;
-  }
-}
-
-quantity.addEventListener("input", validateQuantity); // when user is typing in the quantity input, the function is called.
-
-
+submitBtn.addEventListener("click", validateForm);
 
 
 // Veuillez entrez 2 caractères ou plus pour le champ du prénom.
@@ -126,3 +184,73 @@ quantity.addEventListener("input", validateQuantity); // when user is typing in 
 // Veuillez vérifier que vous accepter les termes et conditions.
 // Vous devez choisir un tournoi.
 // Merci pour votre inscription.
+
+
+// for (let object in formValue) {
+  //   console.log(object, formValue[object]);
+
+  //   if (formValue[object].type == 'text') {
+  //     if (!textRegex.test(formValue[object].value)) {
+  //       console.log('formValue[object]', formValue[object]);
+  //       formValue[object].id.style.border = '2px solid red';
+  //       return false;
+  //     }
+  //     else {
+  //       formValue[object].id.style.border = '2px solid green';
+  //       return true;
+  //     }
+  //   }
+  //   else if (formValue[object].type == 'email') {
+  //     if (!emailRegex.test(formValue[object].value)) {
+  //       formValue[object].id.style.border = '2px solid red';
+  //       return false;
+  //     }
+  //     else {
+  //       formValue[object].id.style.border = '2px solid green';
+  //       return true;
+  //     }
+  //   }
+  //   else if (formValue[object].type == 'date') {
+  //     if (!dateRegex.test(formValue[object].value)) {
+  //       formValue[object].id.style.border = '2px solid red';
+  //       return false;
+  //     }
+  //     else {
+  //       formValue[object].id.style.border = '2px solid green';
+  //       return true;
+  //     }
+  //   }
+  //   else if (formValue[object].type == 'number') {
+  //     if (!numberRegex.test(formValue[object].value)) {
+  //       formValue[object].id.style.border = '2px solid red';
+  //       return false;
+  //     }
+  //     else {
+  //       formValue[object].id.style.border = '2px solid green';
+  //       return true;
+  //     }
+  //   }
+  // }
+
+
+
+
+
+// function validateFirstName(event) { // validate first name : at least 2 characters / not empty
+//   console.log("validateFirstName", event);
+//   let regex = /^[a-zA-Z]{2,}$/;
+//   if (!regex.test(firstName.value)) {
+//     firstName.style.borderColor = "red";
+//     firstName.style.borderWidth = "2px";
+//     return false;
+//   } else {
+//     firstName.style.borderColor = "green";
+//     firstName.style.borderWidth = "2px";
+//     return true;
+//   }
+// }
+
+// firstName.addEventListener("input", validateFirstName); // when user is typing in the first name input, the function is called
+
+
+
