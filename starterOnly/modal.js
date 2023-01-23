9f9b8e844492ea2a7e3aed96f472c8149829e7cc
BaptisteJ24 <baptistejan@outlook.fr>
2023-01-23 19:18:35 +0100

    #2 - Form Entries

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,256 +1,320 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+// DOM elements object
+const
+	modalbg = document.querySelector(".bground"),
+	modalBtn = document.querySelectorAll(".modal-btn"),
+	formDataArray = Array.from(document.querySelectorAll(".formData")),
+	modalForm = document.querySelector(".modal-body form"),
+	modalBody = document.querySelector(".modal-body"),
+	modalBodySuccess = document.querySelector(".modal-body-success"),
+	closeCross = document.querySelector(".close"),
+	closeBtn = document.querySelector(".btn-close"),
+	firstName = document.getElementById("first"),
+	lastName = document.getElementById("last"),
+	email = document.getElementById("email"),
+	birthdate = document.getElementById("birthdate"),
+	quantity = document.getElementById("quantity"),
+	submitBtn = document.getElementById("btn-submit"),
+	radioBtns = document.getElementsByClassName("radio-input"),
+	radioFormData = document.querySelector(`[data-name="location"]`),
+	usingConditions = document.getElementById("checkbox1"),
+	newsletter = document.getElementById("checkbox2"),
+	container = document.querySelectorAll(".container");
 
-// DOM Elements
-const modalbg = document.querySelector(".bground"); // modal background
-const modalBtn = document.querySelectorAll(".modal-btn"); // modal buttons
-const modalForm = document.querySelector(".modal-body form"); // modal form
-const closeCross = document.querySelector(".close"); // close modal
-const closeBtn = document.querySelector(".btn-close"); // close modal button
-const firstName = document.getElementById("first"); // first name
-const lastName = document.getElementById("last"); // last name
-const email = document.getElementById("email"); // email
-const birthdate = document.getElementById("birthdate"); // birthdate
-const quantity = document.getElementById("quantity"); // quantity
-const submitBtn = document.getElementById("btn-submit"); // submit button
-
-const radioBtns = Array.from(document.getElementsByName("location")); // radio buttons
-const radioFormData = document.getElementsByClassName("formData--radioBtn");
-
-const checkbox_UsingConditions = document.getElementById("checkbox1") // checkbox conditions of use
-const checkbox_Newsletter = document.getElementById("checkbox2") // checkbox
-
+// transform formDataArray into an object
+const formData = formDataArray.reduce((acc, element) => { // acc = accumulator
+	acc[element.dataset.name] = element;
+	return acc;
+}, {});
 
 // Regex object
 const regex = {
-  text: /^[a-zA-Z]+[-]?[a-zA-Z]+$/,
-  email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-  date: /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/,
-  number: /^[0-9]{1,2}$/
+	text: /^[a-zA-Z]+[-]?[a-zA-Z]+$/,
+	email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+	date: /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/,
+	number: /^[0-9]{1,2}$/
+}
+
+// function to valid user have 16 years old or more
+function limitAge() {
+	const today = new Date();
+	let birthdateValue = new Date(birthdate.value);
+	if (isNaN(birthdateValue.getTime())) {
+		return false;
+	}
+	else {
+		let age = today.getUTCFullYear() - birthdateValue.getUTCFullYear();
+		let month = today.getUTCMonth() - birthdateValue.getUTCMonth();
+		let day = today.getUTCDate() - birthdateValue.getUTCDate();
+		if (month < 0 || (month === 0 && day < 0)) {
+			age--;
+		}
+		if (age < 16) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
 
 // formData object for each input field in the form : value, type, error, id, valid, regex
-let formData = {
-  firstName: {
-    value: firstName.value,
-    type: firstName.type,
-    error: firstName.dataset.error,
-    id: firstName,
-    valid: false,
-    regex: regex['text']
-  },
-  lastName: {
-    value: lastName.value,
-    type: lastName.type,
-    error: lastName.dataset.error,
-    id: lastName,
-    valid: false,
-    regex: regex['text']
-  },
-  email: {
-    value: email.value,
-    type: email.type,
-    error: email.dataset.error,
-    id: email,
-    valid: false,
-    regex: regex['email']
-  },
-  birthdate: {
-    value: birthdate.value,
-    type: birthdate.type,
-    error: birthdate.dataset.error,
-    id: birthdate,
-    valid: false,
-    regex: regex['date']
-  },
-  quantity: {
-    value: quantity.value,
-    type: quantity.type,
-    error: quantity.dataset.error,
-    id: quantity,
-    valid: false,
-    regex: regex['number']
-  }
+let formData_obj = {
+	firstName: {
+		id: firstName,
+		name: firstName.name,
+		regex: regex['text'],
+		type: firstName.type,
+		valid: false,
+		value: firstName.value
+	},
+	lastName: {
+		id: lastName,
+		name: lastName.name,
+		regex: regex['text'],
+		type: lastName.type,
+		valid: false,
+		value: lastName.value
+	},
+	email: {
+		id: email,
+		name: email.name,
+		regex: regex['email'],
+		type: email.type,
+		valid: false,
+		value: email.value
+	},
+	birthdate: {
+		id: birthdate,
+		name: birthdate.name,
+		regex: regex['date'],
+		type: birthdate.type,
+		valid: false,
+		value: birthdate.value
+	},
+	quantity: {
+		id: quantity,
+		name: quantity.name,
+		regex: regex['number'],
+		type: quantity.type,
+		valid: false,
+		value: quantity.value
+	}
+}
+
+let formData_obj_radio = {};
+for (let i = 0; i < radioBtns.length; i++) {
+	let location = "location" + (i + 1);
+	formData_obj_radio[location] = {
+		name: radioBtns[i].name,
+		id: radioBtns[i],
+		valid: false,
+		value: radioBtns[i].checked,
+	};
+}
+
+let formData_obj_checkbox = {
+	usingConditions: {
+		name: usingConditions.name,
+		error: usingConditions.dataset.error,
+		id: usingConditions,
+		valid: false,
+		value: usingConditions.checked,
+	},
+	newsletter: {
+		value: newsletter.checked,
+		id: newsletter,
+		valid: false
+	}
+}
+
+function editNav() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
 }
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+
+// launch modal function
 function launchModal() {
-  modalbg.style.display = "block";
+	modalbg.classList.toggle("--hide", false);
 }
 
-// close modal event
+
+// close modal events
 closeCross.addEventListener("click", closeModal);
-closeBtn.addEventListener("click", closeModal);
+closeBtn.addEventListener("click", closeModalBtn);
 
-// close modal form
+
+// close modal function when clicking on the cross
 function closeModal() {
-  modalbg.style.display = "none"; // hide modal
-  modalForm.reset(); // reset form
-  Object.keys(formData).forEach((field) => { // loop through formData object
-    formData[field].id.style.border = "none"; // reset border of input fields
-  });
-  modalForm.style.display = "block";
-  document.querySelector(".modal-body-sucess").style.display = "none";
+	modalbg.classList.toggle("--hide", true);
+	modalForm.reset();
+	for (let field in formData_obj) {
+		formData_obj[field].id.classList.toggle("text-control--valid", false);
+	}
 }
 
-// event listener for each input field and green border if is valid
-Object.keys(formData).forEach((field) => { // loop through formData object
-  formData[field].id.addEventListener("focusout", (e) => { // event listener for each input field
-    validateField(e.target, formData[field]); // validate input field
-  });
-});
-
-function validateField(field, fieldData) {
-  if (fieldData.regex.test(field.value)) {
-    field.style.border = "2px solid green";
-    fieldData.valid = true;
-  } else {
-    field.style.border = "none";
-    fieldData.valid = false;
-  }
+// close modal function when clicking on the close button
+function closeModalBtn() {
+	closeModal();
+	closeCross.classList.toggle("--hide", false);
+	modalBodySuccess.classList.toggle("--hide", true);
+	modalBody.classList.toggle("--hide", false);
 }
 
 // initialize formData with the values entered in the form
-const initFormData = () => {
-  for (let object in formData) {
-    formData[object].value = formData[object].id.value;
-  }
+function initFormData() {
+	for (let field in formData_obj) {
+		formData_obj[field].value = formData_obj[field].id.value;
+	}
+	for (let field in formData_obj_radio) {
+		formData_obj_radio[field].value = formData_obj_radio[field].id.checked;
+	}
+	for (let field in formData_obj_checkbox) {
+		formData_obj_checkbox[field].value = formData_obj_checkbox[field].id.checked;
+	}
+}
+
+function validateFormData() {
+	let validDataFields = false;
+
+	// check if firstname, lastname, email, birthdate and quantity of participations are valid (with regex)
+	for (let field in formData_obj) {
+		formData[formData_obj[field].name].dataset.errorVisible = "false";
+		if (!formData_obj[field].regex.test(formData_obj[field].value)) {
+			formData[formData_obj[field].name].dataset.errorVisible = "true";
+			formData_obj[field].valid = false;
+		} else {
+			formData[formData_obj[field].name].dataset.errorVisible = "false";
+			formData_obj[field].valid = true;
+		}
+	}
+
+	// check if birthdate is valid (must be > 16 years old)
+	if (formData_obj.birthdate.valid) {
+		if (limitAge()) {
+			formData.birthdate.dataset.errorVisible = "false";
+			formData_obj.birthdate.valid = true;
+		} else {
+			formData.birthdate.dataset.errorVisible = "true";
+			formData_obj.birthdate.valid = false;
+		}
+	}
+
+	// check if quantity is valid (must be > 0)
+	if (formData_obj.quantity.valid && parseInt(formData_obj.quantity.value) <= 0) {
+		formData.quantity.dataset.errorVisible = "true";
+		formData_obj.quantity.valid = false;
+	}
+
+	// check if all fields are valid
+	for (let field in formData_obj) {
+		if (formData_obj[field].valid) {
+			validDataFields = true;
+		}
+		else {
+			validDataFields = false;
+			break;
+		}
+	}
+
+	// return true if all fields are valid and false if not
+	return validDataFields;
+}
+
+function validateFormData_radio() {
+	let validLocation = false;
+	for (let field in formData_obj_radio) {
+		formData_obj_radio[field].id.checked ?
+			(formData_obj_radio[field].valid = true,
+				validLocation = true) :
+			(formData_obj_radio[field].valid = false);
+	}
+
+	validLocation ?
+		(radioFormData.dataset.errorVisible = "false") :
+		(radioFormData.dataset.errorVisible = "true")
+
+	return validLocation;
+}
+
+function validateUsingConditions() {
+	let validUsingConditions = false;
+
+	formData_obj_checkbox['usingConditions'].id.checked ?
+		(formData[formData_obj_checkbox['usingConditions'].name].dataset.errorVisible = "false",
+			formData_obj_checkbox['usingConditions'].valid = true,
+			validUsingConditions = true) :
+		(formData[formData_obj_checkbox['usingConditions'].name].dataset.errorVisible = "true")
+
+	return validUsingConditions;
+}
+
+function validateNewsletter() {
+	let validNewsletter = false;
+	formData_obj_checkbox['newsletter'].id.checked ?
+		(formData_obj_checkbox['newsletter'].valid = true,
+			validNewsletter = true) :
+		(formData_obj_checkbox['newsletter'].valid = false)
+	return validNewsletter;
 }
 
 // validate form and display error message if necessary. Else, display success message.
-const validateForm = (e) => {
-  e.preventDefault();
-  initFormData();
+function validateForm(e) {
+	e.preventDefault();
+	initFormData();
 
-  let validAll = 0;
+	if (validateFormData() && validateFormData_radio() && validateUsingConditions()) {
+		closeCross.classList.toggle("--hide", true);
+		modalBody.classList.toggle("--hide", true);
+		modalBodySuccess.classList.toggle("--hide", false);
 
-  for (let object in formData) {
-    formData[object].id.style.border = 'none';
-    if (!formData[object].regex.test(formData[object].value)) {
-      formData[object].id.style.border = '2px solid red';
-      return false;
-    }
-    else {
-      formData[object].id.style.border = '2px solid green';
-      validAll++;
-    }
-  }
-
-  if (validAll == 5) {
-    radioBtns.forEach(function (radioBtn) {
-      if (radioBtn.checked) {
-        validAll++;
-        radioBtn.labels[0].querySelector(".checkbox-icon").classList.toggle("checkbox-icon--error", false);
-      }
-    });
-    if (validAll != 6) {
-      radioBtns.forEach(function (radioBtn) {
-        radioBtn.labels[0].querySelector(".checkbox-icon").classList.toggle("checkbox-icon--error", true);
-      });
-      radioFormData.innerHTML += "Vous devez choisir un tournoi auquel participer.";
-      return false;
-    }
-  }
-
-  if (validAll == 6 && checkbox_UsingConditions.checked == true) {
-    modalForm.style.display = "none";
-    document.querySelector(".modal-body-sucess").style.display = "block";
-    if (checkbox_Newsletter.checked == true) {
-      console.log(document.querySelector(".modal-body-sucess").querySelector(".modal-text"));
-      document.querySelector(".modal-body-sucess").querySelector(".modal-text").style.display = "block";
-    }
-  }
-
-
+		if (validateNewsletter()) {
+			modalBodySuccess.querySelector(".modal-text[data-name='modal-text-newsletter']").classList.toggle("--hide", false);
+		}
+		else {
+			modalBodySuccess.querySelector(".modal-text[data-name='modal-text-newsletter']").classList.toggle("--hide", true);
+		}
+	}
 }
 
 submitBtn.addEventListener("click", validateForm);
 
+// event listener for each input field and green border if is valid
+for (let field in formData_obj) {
+	formData_obj[field].id.addEventListener("focusout", (e) => {
+		validateField(e.target, formData_obj[field]);
+	});
+}
 
-// Veuillez entrez 2 caractères ou plus pour le champ du prénom.
-// Veuillez entrez 2 caractères ou plus pour le champ du nom.
-// Veuillez entrer une adresse e-mail valide.
-// Veuillez entrer votre date de naissance.
-// Veuillez entrer un nombre entre 0 et 99.
-// Veuillez vérifier que vous accepter les termes et conditions.
-// Vous devez choisir un tournoi.
-// Merci pour votre inscription.
+// validate field function : green border if is valid
+function validateField(field, fieldData) {
+	if (fieldData.regex.test(field.value)) {
+		field.classList.toggle("text-control--valid", true);
+		formData[field.name].dataset.errorVisible = "false";
+		fieldData.valid = true;
+	} else {
+		field.classList.toggle("text-control--valid", false);
+		formData[field.name].dataset.errorVisible = "true";
+		fieldData.valid = false;
+	}
 
-
-// for (let object in formValue) {
-  //   console.log(object, formValue[object]);
-
-  //   if (formValue[object].type == 'text') {
-  //     if (!textRegex.test(formValue[object].value)) {
-  //       console.log('formValue[object]', formValue[object]);
-  //       formValue[object].id.style.border = '2px solid red';
-  //       return false;
-  //     }
-  //     else {
-  //       formValue[object].id.style.border = '2px solid green';
-  //       return true;
-  //     }
-  //   }
-  //   else if (formValue[object].type == 'email') {
-  //     if (!emailRegex.test(formValue[object].value)) {
-  //       formValue[object].id.style.border = '2px solid red';
-  //       return false;
-  //     }
-  //     else {
-  //       formValue[object].id.style.border = '2px solid green';
-  //       return true;
-  //     }
-  //   }
-  //   else if (formValue[object].type == 'date') {
-  //     if (!dateRegex.test(formValue[object].value)) {
-  //       formValue[object].id.style.border = '2px solid red';
-  //       return false;
-  //     }
-  //     else {
-  //       formValue[object].id.style.border = '2px solid green';
-  //       return true;
-  //     }
-  //   }
-  //   else if (formValue[object].type == 'number') {
-  //     if (!numberRegex.test(formValue[object].value)) {
-  //       formValue[object].id.style.border = '2px solid red';
-  //       return false;
-  //     }
-  //     else {
-  //       formValue[object].id.style.border = '2px solid green';
-  //       return true;
-  //     }
-  //   }
-  // }
-
-
-
-
-
-// function validateFirstName(event) { // validate first name : at least 2 characters / not empty
-//   console.log("validateFirstName", event);
-//   let regex = /^[a-zA-Z]{2,}$/;
-//   if (!regex.test(firstName.value)) {
-//     firstName.style.borderColor = "red";
-//     firstName.style.borderWidth = "2px";
-//     return false;
-//   } else {
-//     firstName.style.borderColor = "green";
-//     firstName.style.borderWidth = "2px";
-//     return true;
-//   }
-// }
-
-// firstName.addEventListener("input", validateFirstName); // when user is typing in the first name input, the function is called
-
-
-
+	if (fieldData.name === "birthdate") {
+		if (limitAge()) {
+			formData.birthdate.dataset.errorVisible = "false";
+			birthdate.classList.toggle("text-control--valid", true);
+			formData_obj.birthdate.valid = true;
+			formData.birthdate.dataset.error = "Veuillez entrer votre date de naissance.";
+		} else {
+			formData.birthdate.dataset.errorVisible = "true";
+			formData.birthdate.dataset.error = "L'inscription est réservée aux personnes de plus de 16 ans.";
+			birthdate.classList.toggle("text-control--valid", false);
+			formData_obj.birthdate.valid = false;
+		}
+	}
+}
